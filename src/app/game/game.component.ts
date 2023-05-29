1fbd05e83460b966a3f7d50ecd28b7cdcad8d523
Real-istic <RealSkaarj@gmx.de>
2023-05-29 20:22:43 +0200

    more player things

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Game } from 'src/models/game'
+import { Game } from 'src/models/game';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
 
 
 @Component({
@@ -11,7 +13,7 @@ export class GameComponent implements OnInit {
   pickCardAnimation = false;
   currentCard: string | any = '';
   game = new Game;
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.newGame();
@@ -31,6 +33,14 @@ export class GameComponent implements OnInit {
         this.pickCardAnimation = false;
       }, 1500);
     }
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogAddPlayerComponent);
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      this.game.players.push(name);
+    });
   }
 
 }
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.scss']
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
+
+  @Input() name: string | undefined;
+  @Input() playerActive: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
