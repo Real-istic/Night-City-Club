fe84074d59c95b8a69ba5be09d2aebee932489c3
Real-istic <RealSkaarj@gmx.de>
2023-05-30 11:33:27 +0200

    namechange

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ringoffire'`, () => {
+  it(`should have as title 'nightcity'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ringoffire');
+    expect(app.title).toEqual('nightcity');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ringoffire app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('nightcity app is running!');
   });
 });
